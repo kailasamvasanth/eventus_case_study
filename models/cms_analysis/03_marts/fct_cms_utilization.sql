315e255874d11d8bd852fcad1fb2d36c15738a2f
vasanthkailash@gmail.com <vasanthkailash@gmail.com>
2026-05-03 18:15:16 +0000

    update

--- models/cms_analysis/03_marts/fct_cms_utilization.sql
+++ models/cms_analysis/03_marts/fct_cms_utilization.sql
@@ -10,16 +10,40 @@ dim_geo AS (
 
 dim_hcpcs AS (
     SELECT * FROM {{ ref('dim_hcpcs') }}
+),
+
+joined AS (
+    SELECT 
+        g.geo_key,
+        g.state_name,
+        h.hcpcs_key,
+        h.service_category,
+        s.tot_bene_day_srvcs
+    FROM staging s
+    JOIN dim_geo g 
+        ON s.state_name = g.state_name
+    JOIN dim_hcpcs h 
+        ON s.hcpcs_cd = h.hcpcs_cd 
+        AND s.place_of_srvc = h.place_of_srvc
+),
+
+aggregated AS (
+    SELECT
+        geo_key,
+        state_name,
+        -- Numerator: Number of AWV Services
+        SUM(CASE WHEN service_category = 'Wellness' THEN tot_bene_day_srvcs ELSE 0 END) as awv_volume,
+        -- Denominator: LTC Encounters
+        SUM(CASE WHEN service_category IN ('LTC Routine', 'ALF Routine') THEN tot_bene_day_srvcs ELSE 0 END) as ltc_encounters
+    FROM joined
+    GROUP BY 1, 2
 )
 
-SELECT 
-    g.geo_key,
-    h.hcpcs_key,
-    s.tot_bene_day_srvcs
-FROM staging s
--- Join back to dimensions to grab the surrogate keys
-JOIN dim_geo g 
-    ON s.state_name = g.state_name
-JOIN dim_hcpcs h 
-    ON s.hcpcs_cd = h.hcpcs_cd 
-    AND s.place_of_srvc = h.place_of_srvc
+SELECT
+    geo_key,
+    state_name,
+    awv_volume,
+    ltc_encounters,
+    -- Handle division by zero gracefully using Snowflake's DIV0 function
+    DIV0(awv_volume, ltc_encounters) as awv_penetration_rate
+FROM aggregated
